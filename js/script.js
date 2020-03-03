5c0d4c183078d312858f38674536da0f3cb6d35f
ndonye mark <ndonyemark@gmail.com>
2020-03-03 06:05:36 -0500

    configure the script to take input from the form

--- js/script.js
+++ js/script.js
@@ -7,5 +7,10 @@ $(document).ready(function(){
     });
     $(".deliver form").submit(function(event){
         event.preventDefault();
+
+        var pizza_size = $("select#size option:selected").val();
+        var pizza_topping = $("select#topping option:selected").val();
+        var pizza_crust = $("select#crust option:selected").val(); 
+        var pizzas = $("input#no_of_pizza").val();
     });
 });
