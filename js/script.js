f7b0753461b3213362cf4e00971e4c7d2247045e
ndonye mark <ndonyemark@gmail.com>
2020-03-03 06:12:40 -0500

    add control structures for the size, crust and topping prices based on the choice the user makes

--- js/script.js
+++ js/script.js
@@ -25,7 +25,44 @@ $(document).ready(function(){
         var pizza_size_price = 0;
         var pizza_topping_price = 0;
         var pizza_crust_price = 0;
-
         
+        if(pizza1.size == 'small')
+        {
+            pizza_size_price += 200;
+        }
+        else if(pizza1.size == "medium")
+        {
+            pizza_size_price += 600;
+        }
+        else if(pizza1.size == "large")
+         {
+            pizza_size_price += 900;
+        }
+    
+        if(pizza1.topping == "ham" || pizza_topping == "BBQ-chicken" || pizza_topping == "bacon" || pizza_topping =="beef")
+        {
+            pizza_topping_price += 150;
+        }
+        else if(pizza1.topping == "blue-cheese" || pizza_topping == "brie" || pizza_topping == "manchengo" || pizza_topping =="parmesan")
+        {
+            pizza_topping_price += 200;
+        }
+        else if(pizza1.topping == "carrots" || pizza_topping == "tomatoes" || pizza_topping == "peas" || pizza_topping =="olives")
+        {
+            pizza_topping_price += 100;
+        }
+    
+        if(pizza1.crust == 'crispy')
+        {
+            pizza_crust_price += 50;
+        }
+        else if(pizza1.crust == "stuffed")
+        {
+            pizza_crust_price += 80;
+        }
+        else if(pizza1.crust == "gluten-free")
+        {
+            pizza_crust_price += 70;
+        }
     });
 });
